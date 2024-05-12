4ed36e2f18c3b20498c8c9db03ba417d92f06b9b
fazzaamiarso <fazzaamiarso@gmail.com>
2024-05-12 23:09:28 +0700

    feat: implement client side votes tracking logic

--- app/(main)/audio-item.tsx
+++ app/(main)/audio-item.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-} from "@/components/ui/card";
-import { useEffect, useRef } from "react";
+import { Button } from "@/components/ui/button";
+import { useEffect, useRef, useState } from "react";
+import localForage from "localforage";
+import { cn } from "@/lib/utils";
+
+const STORAGE_KEY = "votes_state";
 
 export default function ListItem({
   soundKey,
@@ -15,7 +14,14 @@ export default function ListItem({
   soundKey: string;
   description: string;
 }) {
+  const [voteState, setVoteState] = useState<any>({
+    upvotes: [],
+    downvotes: [],
+  });
   const audioRef = useRef<HTMLAudioElement>(null);
+
+  const hasUpvoted = voteState.upvotes.includes(soundKey);
+  const hasDownvoted = voteState.downvotes.includes(soundKey);
 
   useEffect(() => {
     let audioUrl: any;
@@ -38,18 +44,67 @@ export default function ListItem({
     };
   }, [soundKey]);
 
+  useEffect(() => {
+    localForage.getItem(STORAGE_KEY).then((data) => {
+      if (!data) return;
+
+      setVoteState(data);
+    });
+  }, []);
+
+  const upvoteHandler = async () => {
+    const newState = {
+      downvotes: hasDownvoted
+        ? voteState.downvotes.filter((key: string) => key !== soundKey)
+        : voteState.downvotes,
+      upvotes: hasUpvoted
+        ? voteState.upvotes.filter((key: string) => key !== soundKey)
+        : [...voteState?.upvotes, soundKey],
+    };
+
+    await localForage.setItem(STORAGE_KEY, newState);
+    setVoteState(newState);
+  };
+
+  const downvoteHandler = async () => {
+    const newState = {
+      ...voteState,
+      upvotes: hasUpvoted
+        ? voteState.upvotes.filter((key: string) => key !== soundKey)
+        : voteState.upvotes,
+      downvotes: hasDownvoted
+        ? voteState.downvotes.filter((key: string) => key !== soundKey)
+        : [...voteState?.downvotes, soundKey],
+    };
+    await localForage.setItem(STORAGE_KEY, newState);
+    setVoteState(newState);
+  };
+
   return (
-    <li>
-      <Card className="shadow-sm">
-        <CardHeader>
-          <CardDescription>{description}</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div>
-            <audio controls ref={audioRef} className="w-full"></audio>
-          </div>
-        </CardContent>
-      </Card>
+    <li className="flex w-full gap-8 rounded-md bg-white p-4">
+      <div className="flex flex-col items-center">
+        <Button
+          variant="ghost"
+          className={cn("text-xl", hasUpvoted && "saturate-0")}
+          onClick={upvoteHandler}
+        >
+          🔥
+        </Button>
+        <div>0</div>
+        <Button
+          variant="ghost"
+          className={cn("text-xl", hasDownvoted && "saturate-0")}
+          onClick={downvoteHandler}
+        >
+          😓
+        </Button>
+      </div>
+      <div className="basis-full space-y-4">
+        <h3 className="">{description}</h3>
+        <div>
+          <audio controls ref={audioRef} className="w-full"></audio>
+        </div>
+      </div>
     </li>
   );
 }
